61bcac9221a93d003a6040274a463edb21af17fc
Drew Erny <derny@mirantis.com>
2021-01-20 11:12:11 -0600

    Add ability to force colors (for Windows)

--- cmd/root.go
+++ cmd/root.go
@@ -1,10 +1,24 @@
 package cmd
 
 import (
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+)
+
+var (
+	flagColor bool
 )
 
 var RootCmd = &cobra.Command{
 	Use:   "btawiki",
 	Short: "btawiki exports data from BTA3062 game files to mediawiki",
+	PersistentPreRun: func(cmd *cobra.Command, _ []string) {
+		if flagColor {
+			logrus.SetFormatter(&logrus.TextFormatter{ForceColors: true})
+		}
+	},
+}
+
+func init() {
+	RootCmd.PersistentFlags().BoolVar(&flagColor, "color", false, "enable color logging")
 }
